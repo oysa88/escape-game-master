45e632d03c610093960e225542e87686bd9358c5
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2025-11-05 13:14:12 +0100

    Oppdater main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@
 # --------------------------
 gruppe_index = 0
 nivå_index = 0
-basic.show_number(gruppe_index + 1, 0)
-
 # --------------------------
 # KONFIGURASJON
 # --------------------------
+
+ko = ""
+rg = 0
+kodeord: List[str] = []
+radiogruppe: List[List[number]] = []
+
 # Radiogrupper per gruppe (index 0 = Gruppe 1). Hver liste har 5 nivåer.
 radiogruppe = [
     [142, 125, 137, 112, 151],  # Gruppe 1
@@ -17,17 +21,34 @@ radiogruppe = [
     [147, 129, 134, 116, 154],  # Gruppe 5
     [148, 121, 136, 117, 157],  # Gruppe 6
     [144, 123, 131, 118, 156],  # Gruppe 7
-    [141, 124, 135, 119, 158],  # Gruppe 8
-]
+    [141, 124, 135, 119, 158]   # Gruppe 8
+    ]
 
 # Felles kodeord for nivå 1..5 (index 0 = nivå 1)
 kodeord = ["VITENSENTER", "INDIA", "ELEMENTÆRPARTIKKEL", "NEWTON", "MASKINIST"]
-
 radio.set_group(radiogruppe[gruppe_index][nivå_index])
 
+# Vis startstatus
+vis_led_display()
 # --------------------------
 # FUNKSJONER
 # --------------------------
+
+def vis_led_display():
+    basic.clear_screen()
+    y = 0
+    while y < min(gruppe_index + 1, 4):
+        led.plot(0, 4 - y)
+        y += 1
+    if gruppe_index >= 4:
+        y = 0
+        while y < gruppe_index - 3:
+            led.plot(1, 4 - y)
+            y += 1
+    y = 0
+    while y < nivå_index + 1:
+        led.plot(4, 4 - y)
+        y += 1
 
 def on_button_pressed_a():
     global gruppe_index, nivå_index
@@ -35,21 +56,17 @@ def on_button_pressed_a():
     if gruppe_index > 7:
         gruppe_index = 0
     nivå_index = 0
-    basic.show_number(gruppe_index + 1, 0)
-
+    vis_led_display()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-
 def on_button_pressed_b():
-    global gruppe_index, nivå_index
+    global rg, ko, nivå_index
     rg = radiogruppe[gruppe_index][nivå_index]
     ko = kodeord[nivå_index]
-
     radio.set_group(rg)
-    radio.send_string(ko)
-
+    radio.send_string("" + (ko))
     nivå_index += 1
     if nivå_index > 4:
         nivå_index = 0
-
+    vis_led_display()
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -3,12 +3,15 @@
 //  --------------------------
 let gruppe_index = 0
 let nivå_index = 0
-basic.showNumber(gruppe_index + 1, 0)
 //  --------------------------
 //  KONFIGURASJON
 //  --------------------------
+let ko = ""
+let rg = 0
+let kodeord : string[] = []
+let radiogruppe : number[][] = []
 //  Radiogrupper per gruppe (index 0 = Gruppe 1). Hver liste har 5 nivåer.
-let radiogruppe = [[142, 125, 137, 112, 151], [149, 126, 138, 113, 159], [145, 127, 139, 114, 152], [146, 128, 132, 115, 153], [147, 129, 134, 116, 154], [148, 121, 136, 117, 157], [144, 123, 131, 118, 156], [141, 124, 135, 119, 158]]
+radiogruppe = [[142, 125, 137, 112, 151], [149, 126, 138, 113, 159], [145, 127, 139, 114, 152], [146, 128, 132, 115, 153], [147, 129, 134, 116, 154], [148, 121, 136, 117, 157], [144, 123, 131, 118, 156], [141, 124, 135, 119, 158]]
 //  Gruppe 1
 //  Gruppe 2
 //  Gruppe 3
@@ -18,11 +21,35 @@ let radiogruppe = [[142, 125, 137, 112, 151], [149, 126, 138, 113, 159], [145, 1
 //  Gruppe 7
 //  Gruppe 8
 //  Felles kodeord for nivå 1..5 (index 0 = nivå 1)
-let kodeord = ["VITENSENTER", "INDIA", "ELEMENTÆRPARTIKKEL", "NEWTON", "MASKINIST"]
+kodeord = ["VITENSENTER", "INDIA", "ELEMENTÆRPARTIKKEL", "NEWTON", "MASKINIST"]
 radio.setGroup(radiogruppe[gruppe_index][nivå_index])
+//  Vis startstatus
+vis_led_display()
 //  --------------------------
 //  FUNKSJONER
 //  --------------------------
+function vis_led_display() {
+    basic.clearScreen()
+    let y = 0
+    while (y < Math.min(gruppe_index + 1, 4)) {
+        led.plot(0, 4 - y)
+        y += 1
+    }
+    if (gruppe_index >= 4) {
+        y = 0
+        while (y < gruppe_index - 3) {
+            led.plot(1, 4 - y)
+            y += 1
+        }
+    }
+    
+    y = 0
+    while (y < nivå_index + 1) {
+        led.plot(4, 4 - y)
+        y += 1
+    }
+}
+
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     gruppe_index += 1
@@ -31,17 +58,18 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     }
     
     nivå_index = 0
-    basic.showNumber(gruppe_index + 1, 0)
+    vis_led_display()
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    let rg = radiogruppe[gruppe_index][nivå_index]
-    let ko = kodeord[nivå_index]
+    rg = radiogruppe[gruppe_index][nivå_index]
+    ko = kodeord[nivå_index]
     radio.setGroup(rg)
-    radio.sendString(ko)
+    radio.sendString("" + ko)
     nivå_index += 1
     if (nivå_index > 4) {
         nivå_index = 0
     }
     
+    vis_led_display()
 })
